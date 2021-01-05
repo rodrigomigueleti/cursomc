db63770c7029b8b7933eeff201073afd48eb7c28
Migueleti <rodrigo.alvesmigueleti@gmail.com>
2021-01-04 22:36:39 -0300

    Projeto criado

---
+++ src/main/java/com/aulamc/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.aulamc.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
