7fd5bf49f8d7f62e014e8f3856f2c7f62b859693
Rodrigo Migueleti <migueleti@MacBook-Air-de-Rodrigo.local>
2021-02-18 18:45:57 -0300

    Personalizando o retorno de validação

--- src/main/java/com/aulamc/cursomc/dtos/CategoriaDTO.java
+++ src/main/java/com/aulamc/cursomc/dtos/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.aulamc.cursomc.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.aulamc.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/aulamc/cursomc/services/CategoriaService.java
+++ src/main/java/com/aulamc/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.aulamc.cursomc.domain.Categoria;
+import com.aulamc.cursomc.dtos.CategoriaDTO;
 import com.aulamc.cursomc.repositories.CategoriaRepository;
 import com.aulamc.cursomc.services.exceptions.DataIntegrityException;
 import com.aulamc.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
