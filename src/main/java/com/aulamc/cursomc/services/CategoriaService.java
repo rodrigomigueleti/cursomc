9b3f4480e8cbc6f9cecd5758ca6e58b42e2716ac
Rodrigo Migueleti <migueleti@MacBook-Air-de-Rodrigo.local>
2021-02-18 14:38:57 -0300

    deletando categoria e tratando integridade

--- src/main/java/com/aulamc/cursomc/services/CategoriaService.java
+++ src/main/java/com/aulamc/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.aulamc.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.aulamc.cursomc.domain.Categoria;
 import com.aulamc.cursomc.repositories.CategoriaRepository;
+import com.aulamc.cursomc.services.exceptions.DataIntegrityException;
 import com.aulamc.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contenha produtos");
+		}
 	}
 }
