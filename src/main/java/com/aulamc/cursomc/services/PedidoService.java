e56f9a3121c262d797b359eade5b252a0921ff32
Rodrigo Migueleti <migueleti@MacBook-Air-de-Rodrigo.local>
2021-02-17 21:58:58 -0300

    Resource e Service de ItemPedido

---
+++ src/main/java/com/aulamc/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.aulamc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aulamc.cursomc.domain.Pedido;
+import com.aulamc.cursomc.repositories.PedidoRepository;
+import com.aulamc.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! id " + id + " Tipo: " + Pedido.class.getName()
+				));
+	}
+	
+}
